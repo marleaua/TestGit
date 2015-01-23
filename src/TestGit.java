3fe6a15ff1bfe02645fadf63475e5993a331d140
Alexandre Marleau <marleau.alexandre@courrier.uqam.ca>
2015-01-22 23:37:20 -0300

    Ajouté 'Changement commit Bla Bla Bla' à partir du clone de TestGit

--- src/TestGit.java
+++ src/TestGit.java
@@ -25,6 +25,7 @@ public class TestGit {
         System.out.println("Hello " + args[0] + "!");
         System.out.println("Changement commit 1");
         System.out.println("Changement commit 2");
+        System.out.println("Changement commit Bla Bla Bla");
         
     }
     
