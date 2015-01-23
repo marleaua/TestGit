7f790267ef82c11a98e444373d276e01a275ca18
Alexandre Marleau <marleau.alexandre@courrier.uqam.ca>
2015-01-22 23:01:58 -0300

    Ajouté Sop "Bonjour toi là!" à la classe main 'TestGit'

--- src/TestGit.java
+++ src/TestGit.java
@@ -16,6 +16,9 @@ public class TestGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Bonjour toi là!");
+        
     }
     
 }
